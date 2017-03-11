88627a3a722869044510921fdcda6948df8fdf25
Andy Wickert <andy@northernwidget.com>
2017-03-10 18:55:00 -0600

    library include

--- iceflow/core.py
+++ iceflow/core.py
@@ -973,6 +973,7 @@ class IceFlow(object):
     np.save(output_filename + '_DataComparison', data_model_comparison)
     
   def saveGRASS(self):
+    from grass.script import array as garray
     iceGA = garray.array()
     iceGA[...] = self.H
     iceGA.write(self.OutNameGRASS+'_Hice', overwrite=True)
